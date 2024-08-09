dc1aae435051f78fdd3b9b40c663876caa136b90
Luke A. Guest <laguest@archeia.com>
2024-08-09 10:55:43 +0100

    Remove type for now, can't get the thing to compile with the type.

--- src/utils/maths-utils.ads
+++ src/utils/maths-utils.ads
@@ -9,10 +9,10 @@ package Maths.Utils is
    package Trig is new Ada.Numerics.Generic_Elementary_Functions (Float);
 
    --  TODO: Should Radians be a type?
-   --  TODO: Will Degrees cause an exception?
-   type Degrees is range 0.0 .. 360.0;
+   --  TODO: Will Degrees cause an exception? For rotations which increase over 360°?
+   --  type Degrees is digits 3 range 0.0 .. 360.0;
 
-   function To_Radians (Angle : Degrees) return Float is (Degrees * (Ada.Numerics.Pi / 180.0));
-   function To_Degrees (Radians : Float) return Degrees is (Radians * (180.0 / Ada.Numerics.Pi));
+   function To_Radians (Angle_Degrees : Float) return Float is (Angle_Degrees * (Ada.Numerics.Pi / 180.0));
+   function To_Degrees (Angle_Radians : Float) return Float is (Angle_Radians * (180.0 / Ada.Numerics.Pi));
 end Maths.Utils;
 
