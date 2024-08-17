83de2c9d15e84f72604ede781901d198533e47f7
Luke A. Guest <laguest@archeia.com>
2024-08-17 18:50:07 +0100

    Copy over the new Matrix4 code and modify for Matrix 3, untested. Add Translate for 2D x, y coords.

--- src/matrices/maths-matrix3s.adb
+++ src/matrices/maths-matrix3s.adb
@@ -1,43 +1,101 @@
 ------------------------------------------------------------------------------------------------------------------------
 --  This source code is subject to the BSD license, see the LICENCE file in the root of this directory.
 ------------------------------------------------------------------------------------------------------------------------
+with Ada.Numerics.Generic_Elementary_Functions;
+with Ada.Strings.Fixed;
+with Ada.Text_IO;
+
 package body Maths.Matrix3s is
    use type Vector3s.Vector3;
 
-   function "+" (Left, Right : Matrix3) return Matrix3 is
+   package Trig is new Ada.Numerics.Generic_Elementary_Functions (Float);
+   use Trig;
+
+
+   function "+" (L, R : Matrix3) return Matrix3 is
    begin
       return M : Matrix3 (Vectors) do
-         M.Axes (X) := Left.Axes (X) + Right.Axes (X);
-         M.Axes (Y) := Left.Axes (Y) + Right.Axes (Y);
-         M.Axes (Z) := Left.Axes (Z) + Right.Axes (Z);
+         M.Axes (Right)   := L.Axes (Right) + R.Axes (Right);
+         M.Axes (Up)      := L.Axes (Up) + R.Axes (Up);
+         M.Axes (Forward) := L.Axes (Forward) + R.Axes (Forward);
       end return;
    end "+";
 
 
-   function "-" (Left, Right : Matrix3) return Matrix3 is
+   function "-" (L, R : Matrix3) return Matrix3 is
    begin
       return M : Matrix3 (Vectors) do
-         M.Axes (X) := Left.Axes (X) - Right.Axes (X);
-         M.Axes (Y) := Left.Axes (Y) - Right.Axes (Y);
-         M.Axes (Z) := Left.Axes (Z) - Right.Axes (Z);
+         M.Axes (Right)   := L.Axes (Right) - R.Axes (Right);
+         M.Axes (Up)      := L.Axes (Up) - R.Axes (Up);
+         M.Axes (Forward) := L.Axes (Forward) - R.Axes (Forward);
       end return;
    end "-";
 
 
-   function "*" (Left : Matrix3; Right : Vector3s.Vector3) return Vector3s.Vector3 is
+   function "*" (L : Matrix3; R : Vector3s.Vector3) return Vector3s.Vector3 is
    begin
       return V : Vector3s.Vector3 (Vector3s.Components) do
-         V.Elements := (Vector3s.X => Left.Elements (X_Axis_X) * Right.Elements (Vector3s.X) +
-                                      Left.Elements (X_Axis_Y) * Right.Elements (Vector3s.Y) +
-                                      Left.Elements (X_Axis_Z) * Right.Elements (Vector3s.Z),
+         V.Elements := (Vector3s.X => L.Elements (Right_Axis_X) * R.Elements (Vector3s.X) +
+                                      L.Elements (Right_Axis_Y) * R.Elements (Vector3s.Y) +
+                                      L.Elements (Right_Axis_Z) * R.Elements (Vector3s.Z),
 
-                        Vector3s.Y => Left.Elements (Y_Axis_X) * Right.Elements (Vector3s.X) +
-                                      Left.Elements (Y_Axis_Y) * Right.Elements (Vector3s.Y) +
-                                      Left.Elements (Y_Axis_Z) * Right.Elements (Vector3s.Z),
+                        Vector3s.Y => L.Elements (Up_Axis_X) * R.Elements (Vector3s.X) +
+                                      L.Elements (Up_Axis_Y) * R.Elements (Vector3s.Y) +
+                                      L.Elements (Up_Axis_Z) * R.Elements (Vector3s.Z),
 
-                        Vector3s.Z => Left.Elements (Z_Axis_X) * Right.Elements (Vector3s.X) +
-                                      Left.Elements (Z_Axis_Y) * Right.Elements (Vector3s.Y) +
-                                      Left.Elements (Z_Axis_Z) * Right.Elements (Vector3s.Z));
+                        Vector3s.Z => L.Elements (Forward_Axis_X) * R.Elements (Vector3s.X) +
+                                      L.Elements (Forward_Axis_Y) * R.Elements (Vector3s.Y) +
+                                      L.Elements (Forward_Axis_Z) * R.Elements (Vector3s.Z));
+      end return;
+   end "*";
+
+
+   --      Row      * column
+   --      R  U  F     R  U  F
+   ---------------------------
+   --  X | Xx Yx Zx   Ax Bx Cx   XxAx+YxAy+ZxAz XxBx+YxBy+ZxBz XxCx+YxCy+ZxCz
+   --  Y | Xy Yy Zy * Ay By Cy = XyAx+YyAy+ZyAz XyBx+YyBy+ZyBz XyCx+YyCy+ZyCz
+   --  Z | Xz Yz Zz   Az Bz Cz   XzAx+YzAy+ZzAz XzBx+YzBy+ZzBz XzCx+YzCy+ZzCz
+   function "*" (L, R : Matrix3) return Matrix3 is
+      LE : Float_Array renames L.Elements;
+      RE : Float_Array renames R.Elements;
+
+      Xx : Float renames LE (Right_Axis_X);
+      Yx : Float renames LE (Up_Axis_X);
+      Zx : Float renames LE (Forward_Axis_X);
+
+      Xy : Float renames LE (Right_Axis_Y);
+      Yy : Float renames LE (Up_Axis_Y);
+      Zy : Float renames LE (Forward_Axis_Y);
+
+      Xz : Float renames LE (Right_Axis_Z);
+      Yz : Float renames LE (Up_Axis_Z);
+      Zz : Float renames LE (Forward_Axis_Z);
+
+      Ax : Float renames RE (Right_Axis_X);
+      Ay : Float renames RE (Right_Axis_Y);
+      Az : Float renames RE (Right_Axis_Z);
+
+      Bx : Float renames RE (Up_Axis_X);
+      By : Float renames RE (Up_Axis_Y);
+      Bz : Float renames RE (Up_Axis_Z);
+
+      Cx : Float renames RE (Forward_Axis_X);
+      Cy : Float renames RE (Forward_Axis_Y);
+      Cz : Float renames RE (Forward_Axis_Z);
+   begin
+      return M : Matrix3 (Components) do
+         M.Elements := (
+            Right_Axis_X   => (Xx * Ax) + (Yx * Ay) + (Zx * Az),
+            Up_Axis_X      => (Xx * Bx) + (Yx * By) + (Zx * Bz),
+            Forward_Axis_X => (Xx * Cx) + (Yx * Cy) + (Zx * Cz),
+            Right_Axis_Y   => (Xy * Ax) + (Yy * Ay) + (Zy * Az),
+            Up_Axis_Y      => (Xy * Bx) + (Yy * By) + (Zy * Bz),
+            Forward_Axis_Y => (Xy * Cx) + (Yy * Cy) + (Zy * Cz),
+            Right_Axis_Z   => (Xz * Ax) + (Yz * Ay) + (Zz * Az),
+            Up_Axis_Z      => (Xz * Bx) + (Yz * By) + (Zz * Bz),
+            Forward_Axis_Z => (Xz * Cx) + (Yz * Cy) + (Zz * Cz)
+         );
       end return;
    end "*";
 
@@ -45,9 +103,91 @@ package body Maths.Matrix3s is
    function Translate (X, Y : Float) return Matrix3 is
    begin
       return M : Matrix3 (Which => Components) do
-         M.Elements := (Translation_X => X, Translation_Y => Y,
-                        Translation_W | X_Axis_X | Y_Axis_Y => 1.0,
+         M.Elements := (Translation_X => X,
+                        Translation_Y => Y,
+                        Right_Axis_X | Up_Axis_Y | Translation_W => 1.0,
                         others => 0.0);
       end return;
    end Translate;
+
+
+   function Scale (X, Y, Z : Float) return Matrix3 is
+   begin
+      return M : Matrix3 (Which => Components) do
+         M.Elements := (Right_Axis_X   => X,
+                        Up_Axis_Y      => Y,
+                        Forward_Axis_Z => Z,
+                        others         => 0.0);
+      end return;
+   end Scale;
+
+
+   function Rotate_Around_X (Angle : Float) return Matrix3 is
+      Cos_Theta : constant Float := Cos (Angle);
+      Sin_Theta : constant Float := Sin (Angle);
+   begin
+      return M : Matrix3 (Which => Components) do
+         M.Elements := (Right_Axis_X |
+                        Up_Axis_Y      => Cos_Theta,
+                        Up_Axis_Z      => Sin_Theta,
+                        Forward_Axis_Y => -Sin_Theta,
+                        Forward_Axis_Z => Cos_Theta,
+                        others         => 0.0);
+      end return;
+   end Rotate_Around_X;
+
+
+   function Rotate_Around_Y (Angle : Float) return Matrix3 is
+      Cos_Theta : constant Float := Cos (Angle);
+      Sin_Theta : constant Float := Sin (Angle);
+   begin
+      return M : Matrix3 (Which => Components) do
+         M.Elements := (Up_Axis_Y |
+                        Right_Axis_X   => Cos_Theta,
+                        Right_Axis_Z   => -Sin_Theta,
+                        Forward_Axis_X => Sin_Theta,
+                        Forward_Axis_Z => Cos_Theta,
+                        others         => 0.0);
+      end return;
+   end Rotate_Around_Y;
+
+
+   function Rotate_Around_Z (Angle : Float) return Matrix3 is
+      Cos_Theta : constant Float := Cos (Angle);
+      Sin_Theta : constant Float := Sin (Angle);
+   begin
+      return M : Matrix3 (Which => Components) do
+         M.Elements := (Forward_Axis_Z |
+                        Right_Axis_X  => Cos_Theta,
+                        Right_Axis_Y  => Sin_Theta,
+                        Up_Axis_X     => -Sin_Theta,
+                        Up_Axis_Y     => Cos_Theta,
+                        others        => 0.0);
+      end return;
+   end Rotate_Around_Z;
+
+
+   procedure Matrix3_Image (Buffer : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class; Arg : Matrix3) is
+      function Convert (F : Float) return String is
+         use Ada.Strings;
+         use Ada.Strings.Fixed;
+         use Ada.Text_IO;
+
+         package FIO is new Float_IO (Float);
+         use FIO;
+
+         Result : String (1 .. 20) := (others => ' ');
+      begin
+         Put (To   => Result,
+              Item => F,
+              Aft  => 4,
+              Exp  => 0);
+
+         return Trim (Result, Both);
+      end Convert;
+   begin
+      Buffer.Put ("(Right => " & Arg.Axes (Right)'Image &
+                  ", Up => " & Arg.Axes (Up)'Image &
+                  ", Forward => " & Arg.Axes (Forward)'Image & ")");
+   end Matrix3_Image;
 end Maths.Matrix3s;
--- src/matrices/maths-matrix3s.ads
+++ src/matrices/maths-matrix3s.ads
@@ -3,6 +3,7 @@
 ------------------------------------------------------------------------------------------------------------------------
 --  Maths.Matrix3s
 ------------------------------------------------------------------------------------------------------------------------
+with Ada.Strings.Text_Buffers;
 --  with GNAT.SSE.Vector_Types;  --  TODO: Create local versions for portability.
 with Maths.Vector3s;
 
@@ -12,31 +13,47 @@ package Maths.Matrix3s is
    --  package SIMDV renames GNAT.SSE.Vector_Types;
 
    --  Column Matrices.
+   --
+   --  X - Right vector.
+   --  Y - Up vector.
+   --  Z - Forward vector.
+   --  T - Translation vector.
    --                                        Basis
    --                                        Axes
-   --  Axis    Array             Matrix      X Y Z
-   --  ----    ----------    -----------     -----
+   --  Axis    Array         Matrix          R U F
+   --  ----    --------      -----------     -----
    --  X   |   M0 M3 M6      M11 M12 M13     x x x
    --  Y   |   M1 M4 M7  :-> M21 M22 M23 :-> y y y
    --  Z   |   M2 M5 M8      M31 M32 M33     z z z
+   --
+   --  In Ada, using Fortran convention, you still use M (Row, Column) to acess
+   --  individual elements in a 2D matrix.
 
-   type Matrix_Elements is (X_Axis_X, X_Axis_Y, X_Axis_Z,
-                            Y_Axis_X, Y_Axis_Y, Y_Axis_Z,
-                            Z_Axis_X, Z_Axis_Y, Z_Axis_Z);
+   type Matrix_Elements is (Right_Axis_X,   Right_Axis_Y,   Right_Axis_Z,
+                            Up_Axis_X,      Up_Axis_Y,      Up_Axis_Z,
+                            Forward_Axis_X, Forward_Axis_Y, Forward_Axis_Z);
 
-   Translation_X renames X_Axis_Z;
-   Translation_Y renames Y_Axis_Z;
-   Translation_W renames Z_Axis_Z;
+   Translation_X renames Right_Axis_Z;
+   Translation_Y renames Up_Axis_Z;
+   Translation_W renames Forward_Axis_Z;
 
-   type Basis_Axes is (X, Y, Z);
+   type Basis_Axes is
+     (Right,    --  X
+      Up,       --  Y
+      Forward); --  Z
+
+   --  Axes within the basis axes.
    type Axes is (X, Y, Z);
 
-   --  type M128_Array is array (Basis_Axes) of SIMDV.m128;  --  The machine representation.
+   --  type M128_Array is array (Basis_Axes) of SIMDV.m128;  --  The machine representation, 4 x floats.
    type Float_Array is array (Matrix_Elements) of Float;
    type Vector_Array is array (Basis_Axes) of Vector3s.Vector3 (Vector3s.Components);
 
    type Matrix_2D_Array is array (Axes, Basis_Axes) of Float with
      Convention => Fortran;
+
+   type Row_Major_Matrix is new Matrix_2D_Array with
+     Convention => Ada;
 
    type Matrix_Access_Type is (SIMD, Components, Matrix_2D, Vectors);
 
@@ -57,26 +74,37 @@ package Maths.Matrix3s is
       end case;
    end record with
      Convention => C,
-     Unchecked_Union;
+     Unchecked_Union,
+     Put_Image => Matrix3_Image;
 
    Identity : constant Matrix3;
 
    --  Operators.
-   function "+" (Left, Right : Matrix3) return Matrix3 with
+   function "+" (L, R : Matrix3) return Matrix3 with
      Inline;
 
-   function "-" (Left, Right : Matrix3) return Matrix3 with
+   function "-" (L, R : Matrix3) return Matrix3 with
      Inline;
 
-   function "*" (Left : Matrix3; Right : Vector3s.Vector3) return Vector3s.Vector3 with
+   function "*" (L : Matrix3; R : Vector3s.Vector3) return Vector3s.Vector3 with
      Inline;
 
-   --  A 3x3 matrix can be used to translate a 2D vector.
-   function Translate (X, Y : Float) return Matrix3;
+   function "*" (L, R : Matrix3) return Matrix3 with
+     Inline;
+
+   --  function Translate (X, Y, Z : Float) return Matrix3;
+   function Scale (X, Y, Z : Float) return Matrix3;
+   function Rotate_Around_X (Angle : Float) return Matrix3;
+   function Rotate_Around_Y (Angle : Float) return Matrix3;
+   function Rotate_Around_Z (Angle : Float) return Matrix3;
+
+   function Switch_Coordinate_Systems return Matrix3 is (Scale (1.0, 1.0, -1.0));
+
+   procedure Matrix3_Image (Buffer : in out Ada.Strings.Text_Buffers.Root_Buffer_Type'Class; Arg : Matrix3);
 private
    Identity : constant Matrix3 :=
      (Which       => Matrix_2D,
-      Elements_2D => ((1.0, 0.0, 0.0),
-                      (0.0, 1.0, 0.0),
-                      (0.0, 0.0, 1.0)));
+      Elements_2D => ((1.0, 0.0, 0.0),     --  Right.
+                      (0.0, 1.0, 0.0),     --  Up.
+                      (0.0, 0.0, 1.0)));   --  Forward.
 end Maths.Matrix3s;
