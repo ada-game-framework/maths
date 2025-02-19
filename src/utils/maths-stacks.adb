b17c394eb8f9e72ef6240d84b8c730b7ccacb785
Luke A. Guest <laguest@archeia.com>
2025-02-19 09:34:43 +0000

    Add remaining operations.

--- src/utils/maths-stacks.adb
+++ src/utils/maths-stacks.adb
@@ -1,11 +1,54 @@
 ------------------------------------------------------------------------------------------------------------------------
 --  This source code is subject to the BSD license, see the LICENCE file in the root of this directory.
 ------------------------------------------------------------------------------------------------------------------------
+with Ada.Unchecked_Deallocation;
+
 package body Maths.Stacks is
-   procedure Init (Self : in out Stack) is
+   procedure Init (Self : in out Stack; Size : Natural := Capacity) is
    begin
-      Self.Elements := new Element_Array (1 .. Capacity);
+      Self.Elements := new Element_Array (1 .. Size);
    end Init;
+
+
+   procedure Push_Top (Self : in out Stack) is
+      S : constant not null access constant Element_Array := Self.Elements;
+   begin
+      Push (Self, S (Self.Current_Top));
+   end Push_Top;
+
+
+   procedure Push (Self : in out Stack; New_Element : Element_Type) is
+      S       : constant not null access Element_Array := Self.Elements;
+      New_Top : constant Natural := Self.Current_Top + 1;
+   begin
+      --  Are we about to exceed the size?
+      if New_Top > S'Last then
+         Resize (Self);
+      end if;
+
+      S (New_Top) := New_Element;
+
+      Self.Current_Top := New_Top;
+   end Push;
+
+
+   --  procedure Push (Self : in out Stack; New_Top : Reference_Type) is
+   --  begin
+   --     Push (Self, )
+   --  end Push;
+
+
+   function Pop (Self : in out Stack) return Element_Type is
+      Result : Element_Type;
+   begin
+      if Self.Current_Top > Natural'First then
+         Result := Self.Elements (Self.Current_Top);
+
+         Self.Current_Top := @ - 1;
+      end if;
+
+      return Result;
+   end Pop;
 
 
    function Top (Self : aliased in out Stack) return Reference_Type is
@@ -20,4 +63,21 @@ package body Maths.Stacks is
    begin
       return Self.Current_Top = Natural'First;
    end Is_Empty;
+
+
+   procedure Resize (Self : in out Stack; Increase_By : Natural := Capacity) is
+      procedure Free is new Ada.Unchecked_Deallocation (Object => Element_Array, Name => Element_Access);
+
+      Temp     : Element_Access := Self.Elements;
+      Temp_Top : Natural        := Self.Current_Top;
+   begin
+      if Self.Elements /= null then
+         Init (Self, Self.Elements'Length + Increase_By);
+
+         Self.Elements (Temp'Range) := Temp.all;
+         Self.Current_Top           := Temp_Top;
+
+         Free (Temp);
+      end if;
+   end Resize;
 end Maths.Stacks;
--- src/utils/maths-stacks.ads
+++ src/utils/maths-stacks.ads
@@ -8,20 +8,26 @@ generic
 
    Capacity : Positive := 10;
 
-   with function "=" (Left, Right : Element_Type)
-      return Boolean is <>;
+   --  with function "=" (Left, Right : Element_Type)
+   --     return Boolean is <>;
 package Maths.Stacks is
-   type Stack is private;
+   type Stack is limited private;
 
    type Reference_Type (Data : not null access Element_Type) is limited private with
      Implicit_Dereference => Data;
 
-   procedure Init (Self : in out Stack);
-   --  procedure Push (Self : in out Stack_Vector.Stack);
-   --  procedure Push (Self : in out Stack_Vector.Stack; New_Top : Element_Type);
-   --  procedure Push (Self : in out Stack_Vector.Stack; New_Top : Reference);
-   --  function Pop (Self : Stack_Vector.Stack) return Element_Type;
-   --  function Top (Self : aliased in out Stack) return Reference;
+   procedure Init (Self : in out Stack; Size : Natural := Capacity);
+
+   --  Push a copy of the top onto the top.
+   procedure Push_Top (Self : in out Stack) with
+     Inline;
+
+   procedure Push (Self : in out Stack; New_Element : Element_Type);
+
+   --  procedure Push (Self : in out Stack; New_Top : Reference_Type);
+
+   function Pop (Self : in out Stack) return Element_Type;
+
    function Top (Self : aliased in out Stack) return Reference_Type with
      Inline;
 
@@ -31,10 +37,12 @@ private
    type Element_Array is array (Positive range <>) of aliased Element_Type;
    type Element_Access is access all Element_Array;
 
-   type Stack is record
+   type Stack is limited record
       Elements    : Element_Access := null;
       Current_Top : Natural        := Natural'First; --  When 0, stack = empty.
    end record;
+
+   procedure Resize (Self : in out Stack; Increase_By : Natural := Capacity);
 
    type Reference_Type (Data : not null access Element_Type) is limited null record;
 end Maths.Stacks;
